f25ec06ef6d1999fa663c488cb6b8d3d14de88db
Mohit Pandey <pandeymohit215@gmail.com>
2023-05-05 20:40:06 +0530

    ques

--- objects/question.js
+++ objects/question.js
@@ -126,22 +126,129 @@ const demo={
  
  
  <----------------------------------------------------------->
-
+ 
  Q8. JSON.stringify  and JSON.parse
     
-    const demo={
+ const demo={
      name:"Mohit Pandey",
      title:"Full stack developer"
     }
 
     const strDemo= JSON.stringify(demo)
+    console.log(strDemo)
     
+    Output:
+    {"name":"Mohit","title":"Full stack developer"}
+
+    const objStrDemo=JSON.parse(strDemo);
+    console.log(objStrDemo)
+    
+    Output:
+    {name: 'Mohit Pandey', title: 'Full stack developer'}
+    name:"Mohit Pandey"
+    title:"Full stack developer"
+    [[Prototype]]:Object
+    
+    
+    Storing values in localstorage
+
+    localStorage.setItem("test",strDemo)
+    console.log(localStorage.getItem("test",strDemo))
+    Output:   test : {"name":"Mohit","title":"Full stack developer"}
+    localStorage assigns string values. if objects are assigned then it stores it as [object Object] 
+    
+    
+    <----------------------------------------------------------->
+    
+    Q9. Spread operator
+
+    console.log([..."Mohit"])
+    (5) ['M', 'o', 'h', 'i', 't']
+
+    const user={name:'Mohit',cool:'true'}
+    const admin={liar:true,...user}
+
+    console.log(admin)
+
+    Output:
+    {liar: true, name: 'Mohit', cool: 'true'}
+    cool:"true"
+    liar:true
+    name:"Mohit"
+    [[Prototype]]:Object
+    
+    
+    <----------------------------------------------------------->
+    
+    Q10. Predict the output
+
+    const settings ={
+        username: "Mohit",
+        level: 21,
+        health: 98
+    }
+
+    const data =JSON.stringify(settings,["level","health"])
+    console.log(data)
+    
+    Output:
+    {"level":21,"health":98}
+    here only the keys specified gets stringified
+
+
+    <----------------------------------------------------------->
+    
+    Q11. Predict the output
+
+    const shape={
+        radius:10,
+        diameter(){
+            return this.radius*2;
+        },
+        perimeter: ()=>2*Math.PI*this.radius
+    }
+    
+    console.log(shape.diameter())
+    console.log(shape.perimeter())
+
+    Output:
+    20
+    NaN -> 'this' keyword in arrow fnc points to window object and as there is no radius, therefore NaN is logged
+    
+    
+    <----------------------------------------------------------->
+    
+    Q12. Destructuring
+
+    let demo ={
+        name: "Mohit",
+        title:"Full stack Developer"
+    }
+
+    const {name}=demo;
+    console.log(name)
+
+    Output:
+    Mohit
 
 
 
- 
- 
- 
- 
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    */
+   
